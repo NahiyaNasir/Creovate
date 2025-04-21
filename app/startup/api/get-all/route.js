421a4e3e38e946af9271a383f514d9859ad5f717
NahiyaNasir <nahiaesha.01@gmail.com>
2025-04-21 13:18:52 +0600

    Fixed env fille

--- app/startup/api/get-all/route.js
+++ app/startup/api/get-all/route.js
@@ -3,7 +3,7 @@
 import { connectDB } from "@/lib/connectDB"
 export const GET=async()=>{
     const db=  await connectDB ()
-     const collection= await  db.collection('startup')
+     const collection=  db.collection('startup')
      try {
      
        const resp= await collection.find().toArray()
